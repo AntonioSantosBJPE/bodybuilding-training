f6df02e8f9aeec911fe90a2d3649c5754b291a48
Antonio Santos <netoifpe@gmail.com>
2024-09-22 10:50:13 -0300

    feat: add initial login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import LoginForm from '@/components/authentication-02'
 import { ModeToggle } from '@/components/theme-toogle'
 
 export default function Home() {
 	return (
-		<section>
-			<ModeToggle />
+		<section className="w-full max-w-7xl mx-auto min-h-screen flex flex-col  gap-2 max-xl:px-6 px-2  ">
+			<header className="w-full flex justify-end py-2">
+				<ModeToggle />
+			</header>
+			<div className="w-full h-full flex-1 flex justify-center items-center">
+				<LoginForm />
+			</div>
 		</section>
 	)
 }
